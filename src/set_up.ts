c53b1555f2122c6629aa67b1c903e0b81282f101
Matt Corich <+@users.noreply.github.com>
2021-04-29 15:45:52 +1200

    missed a file

--- src/set_up.ts
+++ src/set_up.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 import { getSetUpFolders } from './utils/folders';
 import { createFile, createFolder } from './utils/file_util';
-import { getFailuresTemplate, getFixtureReaderTemplate, getUsecaseTemplate } from './templates/core_template';
+import { getFailuresTemplate, getFixtureReaderTemplate, getUsecaseTemplate } from './templates/core_templates';
 
 /**
  * Shows an input box using window.showInputBox().
